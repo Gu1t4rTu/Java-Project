3d4b2495ed660423d2d47ea76e9a507451806320
User <User@User-PC>
2021-04-13 18:44:00 +0300

    Class Player

--- src/main/java/iek/agdimitr/lab07/Player.java
+++ src/main/java/iek/agdimitr/lab07/Player.java
@@ -5,10 +5,22 @@
  */
 package iek.agdimitr.lab07;
 
+import java.util.Date;
+
 /**
  *
  * @author User
  */
 public class Player {
+    private String firstName; //Όνομα Παίκτη
+    private String lastName; //Επίθετο Παίκτη
+    private Date birthDate; //Ημερομηνία Γέννησης Παίκτη
+    
+    public Player(String f, String l){
+        this.firstName = f;
+        this.lastName = l;
+    }
+    //TODO Να φτιάξω μέθοδο για υπολογισμό ηλικίας από ημερομηνία
+    //TODO Να φτιάξω μέθοδο για προσθήκη καλαθιών
     
 }
